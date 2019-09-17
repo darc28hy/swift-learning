67e37787276b903ce32c7d54602e7d95a4a38a38
Hiroki Yoshida <>
2019-09-17 19:35:25 +0900

    「Create Outlets for UI Elements」まで

--- SwiftLearning/ViewController.swift
+++ SwiftLearning/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
